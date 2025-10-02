e4049d93014ed3445e867ca5c5ef07809efb6ddc
Jules MUKADI <146131402+2MJ-DEV@users.noreply.github.com>
2025-10-02 18:42:35 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>OS243</h1>
+        <h1 className="text-5xl font-extrabold leading-[1.1] text-center sm:text-left flex gap-2">
+          <span>CodeTogether</span>
+          <div className="">
+            <span className="text-[#007FFF]">d</span>
+          <span className="text-[#FF0000]">r</span>
+          <span className="text-[#FFD700]">c</span>
+          </div>
+        </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
